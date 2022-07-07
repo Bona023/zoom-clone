65aaf4283e62c82ce9c172790763a18347baf16b
bona <bona2324@gmail.com>
2022-07-07 10:26:35 +0900

    frontend setup
    setup

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 
-console.log("Hello!");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => redirect("/"));
 
-app.listen(3000);
+const handleListening = () => console.log("✅ Server listening on port http://localhost:3000 🛩️");
+app.listen(3000, handleListening);
