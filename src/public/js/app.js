58a6e4ec6c0cc41af462e0fac8ce982c0eda22d3
bona <bona2324@gmail.com>
2022-07-26 16:21:01 +0900

    room, message, nickname
    room입장
    입장, 퇴장 메시지
    메시지 보내기
    nickname 저장하기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,40 +1,57 @@
-const messageList = document.querySelector("ul");
-const nickForm = document.querySelector("#nick");
-const messageForm = document.querySelector("#message");
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = io();
 
-function makeMessage(type, payload) {
-    const msg = { type, payload };
-    return JSON.stringify(msg);
-}
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+const room = document.getElementById("room");
 
-socket.addEventListener("open", () => {
-    console.log("✅ Connected to Server");
-});
+room.hidden = true;
 
-socket.addEventListener("message", (message) => {
+let roomName;
+
+function addMessage(message) {
+    const ul = room.querySelector("ul");
     const li = document.createElement("li");
-    li.innerText = message.data;
-    messageList.append(li);
-});
+    li.innerText = message;
+    ul.appendChild(li);
+}
 
-socket.addEventListener("close", () => {
-    console.log("❌ Disconnected from Server");
-});
-
-function handelSubmit(event) {
+function handleMessageSubmit(event) {
     event.preventDefault();
-    const input = messageForm.querySelector("input");
-    socket.send(makeMessage("new_message", input.value));
+    const input = room.querySelector("#msg input");
+    const value = input.value;
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You: ${value}`);
+    });
     input.value = "";
 }
 
-function handleNickSubmit(event) {
+function showRoom() {
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName}`;
+    const msgForm = room.querySelector("#msg");
+    msgForm.addEventListener("submit", handleMessageSubmit);
+}
+
+function handleRoomSubmit(event) {
     event.preventDefault();
-    const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname", input.value));
+    const enterRoom = form.querySelector("#enter_room");
+    const nickname = form.querySelector("#name");
+    socket.emit("nickname", nickname.value);
+    socket.emit("enter_room", enterRoom.value, showRoom);
+    roomName = enterRoom.value;
     input.value = "";
 }
 
-messageForm.addEventListener("submit", handelSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", (user) => {
+    addMessage(`${user} arrived!`);
+});
+
+socket.on("bye", (user) => {
+    addMessage(`${user} left ㅠㅠ`);
+});
+
+socket.on("new_message", addMessage);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import { Server } from "socket.io";
 import express from "express";
 
 const app = express();
@@ -10,8 +10,33 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log("✅ Server listening on port http://localhost:3000 🛩️");
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
 
+wsServer.on("connection", (socket) => {
+    socket["nickname"] = "Anon";
+    socket.onAny((event) => {
+        console.log(`socket Event: ${event}`);
+    });
+    socket.on("enter_room", (roomName, done) => {
+        socket.join(roomName);
+        done();
+        socket.to(roomName).emit("welcome", socket.nickname);
+    });
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname));
+    });
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+        done();
+    });
+    socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
+});
+
+const handleListen = () => console.log("✅ Server listening on port http://localhost:3000 🛩️");
+httpServer.listen(3000, handleListen);
+
+/* WebSocket을 사용해서 만든 서버 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
@@ -33,6 +58,7 @@ wss.on("connection", (socket) => {
                 break;
         }
     });
-});
+}); 
 
 server.listen(3000, handleListen);
+*/
