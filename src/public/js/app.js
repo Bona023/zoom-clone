0bbfb24292d13e02885e1f12dda874ee14c7fffe
bona <bona2324@gmail.com>
2022-08-14 17:25:45 +0900

    Senders

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -79,6 +79,11 @@ function handleCameraBtnClick() {
 
 async function handleCameraChange() {
     await getMedia(camerasSelect.value);
+    if (myPeerConnection) {
+        const videoTrack = myStream.getVideoTracks()[0];
+        const videoSender = myPeerConnection.getSenders().find((sender) => sender.track.kind === "video");
+        videoSender.replaceTrack(videoTrack);
+    }
 }
 
 muteBtn.addEventListener("click", handleMuteBtnClick);
